ccdd2863cbb08bed5776c1225b3ebc41425a4aa5
DavidTrabuco <davidtrabuco88@gmail.com>
2026-09-02 07:12:29 -0300

    Adicionando configurações de CORS

--- BankingApi/Program.cs
+++ BankingApi/Program.cs
@@ -5,24 +5,33 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Adiciona os serviços básicos da Web API ao container do ASP.NET Core
+// 1. Configurações de Serviços e Dependências
+var frontendUrl = builder.Configuration["FrontendSettings:AllowedUrl"] ?? "http://localhost:5173";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LiberarFrontend", policy =>
+    {
+        policy.WithOrigins(frontendUrl!)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BancoDbContext>(options => options.UseSqlite("Data Source=banco.db"));
-// 2. REGISTRO DA INJEÇÃO DE DEPENDÊNCIA (DI):
-// Diz ao .NET que sempre que alguma classe pedir um "INotificador", 
-// ele deve entregar automaticamente uma instância de "NotificadorEmail".
+
+// 2. Registro da Injeção de Dependência (DI)
 builder.Services.AddScoped<INotificador, NotificadorEmail>();
 builder.Services.AddScoped<TransacaoServices>();
 builder.Services.AddScoped<ClienteService>();
 builder.Services.AddScoped<ContaService>();
 
-
-
 var app = builder.Build();
 
-// 3. Configura a interface interativa do Swagger para testarmos a API no navegador
+// 3. Middlewares do Pipeline HTTP (A ORDEM IMPORTA!)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -30,9 +39,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// O CORS DEVE vir ANTES do UseAuthorization e do MapControllers
+app.UseCors("LiberarFrontend");
+
 app.UseAuthorization();
 
-// 4. Mapeia os nossos Controllers para responderem a rotas HTTP (GET, POST, etc.)
 app.MapControllers();
 
 app.Run();
--- BankingApi/obj/Debug/net8.0/BankingApi.AssemblyInfo.cs
+++ BankingApi/obj/Debug/net8.0/BankingApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09fa9776f388d6afd4af944880ecfaf979f2b3b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8dd8c01620c83fe2f2764bee4e16b45a64e07b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
